38ae2d92bbde57fbcd2e95d0da7b4fb93e3b2946
Rasita Vajapattana <122709179+nanarv@users.noreply.github.com>
2023-08-03 11:25:23 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -3,11 +3,22 @@ import cv2
 import numpy as np
 from PIL import Image
 import json
+import time
 #import line
 
 # vid = cv2.VideoCapture(0)
+print("avail",torch.cuda.is_available())
+print("list: \n", torch.cuda.get_arch_list())
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='bestcatanddogbreed.pt') 
-vid = cv2.VideoCapture("cat6.mp4")
+#model.cuda()  # GPU
+#torch.cuda.device(7)
+#device=torch.device(7)
+model.to("cpu")  # i.e. device=torch.device(0)
+vid = cv2.VideoCapture("rtsp://admin:Otmadmin1234@10.10.9.115:554/cam/realmonitor?channel=1&subtype=0")
+vid.set(cv2.CAP_PROP_BUFFERSIZE, 0)
+
+
+
 
 # https://gist.github.com/syanyong/5fd83ff7d006d4566e115f9dbf203905
 def plot_boxes(result_dict, frame):
@@ -25,19 +36,37 @@ def plot_boxes(result_dict, frame):
         cv2.putText(frame, "%s %0.4f" % (ob['name'], ob['confidence']), rec_start, cv2.FONT_HERSHEY_DUPLEX, 2, color, thickness)
     return frame
 ret=True
-while(ret):
-    ret, image = vid.read()
-    # Load the image
-    cv2.imwrite("conversion.jpg", image)
-    results = model("conversion.jpg",size=640)
-    # working with the prediction results to plot boxes
+count=0
+runtime = 0
+previous_runtime=0
+new_runtime=0
+totalfps=0
+while(True):
+    # time start
+    # detection
+    ret, frame = vid.read()
+    cv2.imwrite("conversion.jpg", frame)
+    #frame = cv2.553, vid
+    results = model(frame)
     result_jsons = results.pandas().xyxy[0].to_json(orient="records")
     result_dict = json.loads(result_jsons)
-    print(result_dict)
-    frame2 = plot_boxes(result_dict, image)
-    cv2.imshow('YOLO', frame2)
+    frame2 = plot_boxes(result_dict, frame)
+    #for ob in result_dict:
+      #  print(ob['name'])
+    cv2.imshow('YOLO', frame)
+    # calculate fps
+    new_runtime = time.time()
+    if count !=0:
+        totalfps+=fps
+    count+=1
+    #print("new:",new_runtime, "prev:",previous_runtime, "passed:",new_runtime-previous_runtime)
+    fps = float(1/(new_runtime-previous_runtime))
+    print('Frames per second : ', fps,'FPS')
+    previous_runtime = new_runtime
+
+    # enable quit command
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("avg fps:", totalfps/count-1)
         break
 
-vid.release()
-cv2.destroyAllWindows()
